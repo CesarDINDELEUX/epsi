64598bc3d43bd782012cef3b518605cff5f35832
PutinIsLove <cesar.dindeleux@gmail.com>
2018-04-12 10:02:06 +0200

    Auto stash before merge of "master" and "origin/master"

--- epsiProject/views.py
+++ epsiProject/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, projet_id):
+	return HttpResponse("Projet n° : %s." % projet_id)
